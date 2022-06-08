30ef38dfce85a379061ba4d5122d8d045feabaaf
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2022-06-08 18:12:21 -0500

    Update code

--- get_github_release_log.sh
+++ get_github_release_log.sh
@@ -1,10 +1,17 @@
 git log --pretty=oneline | sed 's/[^ ]* *//' > git_logs.txt
 
+if [[ -f "git_logs_output.txt" ]]; then
+  rm git_logs_output.txt
+fi
+
 while read text_line; do
   if [[ "$text_line" =~ "[🤖 DailyBot] New release to v" ]]; then
     break
   fi
+  if [[ -f "git_logs_output.txt" ]]; then
+    echo -n "\n" >> git_logs_output.txt
+  fi
   if [[ ! "$text_line" =~ "Merge branch 'master'" ]]; then
-    echo "🚩 $text_line" >> git_logs_output.txt
+    echo -n "🚩 $text_line" >> git_logs_output.txt
   fi
 done < git_logs.txt
