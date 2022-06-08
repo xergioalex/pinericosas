223525c0d53428d0e5ba0deb6b80fc1902854242
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2022-06-08 18:21:27 -0500

    Update action

--- get_github_release_log.sh
+++ get_github_release_log.sh
@@ -8,10 +8,7 @@ while read text_line; do
   if [[ "$text_line" =~ "[🤖 DailyBot] New release to v" ]]; then
     break
   fi
-  if [[ -f "git_logs_output.txt" ]]; then
-    echo -n "\n" >> git_logs_output.txt
-  fi
   if [[ ! "$text_line" =~ "Merge branch 'master'" ]]; then
-    echo -n "🚩 $text_line" >> git_logs_output.txt
+    echo "🚩 $text_line" >> git_logs_output.txt
   fi
 done < git_logs.txt
